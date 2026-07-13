8d6602757a2a9c0d125c9c108cca1fb7fd2b4d08
Mateusz "Serafin" Gajewski <github@wendigo.pl>
2026-07-13 13:28:24 +0200

    Add blob cache plugin SPI and cache manager registry
    Move cache control to the engine: blob cache managers are loaded from
    cache-manager.config-files (the memory manager is loaded by default when
    nothing is configured), leased to connectors through
    ConnectorCacheFactory against declared capability requirements, scoped
    per catalog and usage in the cache key space, and dropped when a catalog
    is dropped on the coordinator or pruned on a worker.
    
    (cherry picked from commit f044ba60b9d1641790f3fd9d971598c7c8e66f77)

--- core/trino-main/src/main/java/io/trino/server/Server.java
+++ core/trino-main/src/main/java/io/trino/server/Server.java
@@ -34,6 +34,8 @@ import io.airlift.node.NodeModule;
 import io.airlift.openmetrics.JmxOpenMetricsModule;
 import io.airlift.tracing.TracingModule;
 import io.airlift.units.Duration;
+import io.trino.cache.CacheManagerModule;
+import io.trino.cache.CacheManagerRegistry;
 import io.trino.connector.CatalogManagerModule;
 import io.trino.connector.CatalogStoreManager;
 import io.trino.connector.ConnectorServicesProvider;
@@ -103,6 +105,7 @@ public class Server
                 new TracingModule("trino", trinoVersion),
                 new ServerSecurityModule(),
                 new AccessControlModule(),
+                new CacheManagerModule(),
                 new EventListenerModule(),
                 new ExchangeManagerModule(),
                 new CatalogManagerModule(),
@@ -129,6 +132,8 @@ public class Server
             logLocation(log, "Etc directory", Path.of("etc"));
 
             injector.getInstance(PluginInstaller.class).loadPlugins();
+            // Caches can be requested for initial catalogs so we need to wire these first
+            injector.getInstance(CacheManagerRegistry.class).loadCacheManagers();
 
             var catalogStoreManager = injector.getInstance(Key.get(new TypeLiteral<Optional<CatalogStoreManager>>() {}));
             catalogStoreManager.ifPresent(CatalogStoreManager::loadConfiguredCatalogStore);
