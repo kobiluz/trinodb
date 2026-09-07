wonju-dev/trino

Add alluxio blob cache plugin
Disk-backed page cache built on the alluxio client library, providing
CAN_EXCEED_HEAP_SIZE capacity for table data working sets read by
worker scans. Explicit invalidation is unsupported by the page store
(page ids are one-way hashes of the cache keys), so stale entries are
dropped by TTL and size eviction and keys must be versioned when
content can be overwritten in place.

plugin/trino-blob-cache-alluxio/src/main/java/io/trino/blob/cache/alluxio/AlluxioBlobCacheManager.java
@@ -1,0 +1,121 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.trino.blob.cache.alluxio;
+
+import com.google.inject.Inject;
+import io.airlift.log.Logger;
+import io.trino.spi.cache.Blob;
+import io.trino.spi.cache.BlobCache;
+import io.trino.spi.cache.BlobCacheManager;
+import io.trino.spi.cache.BlobSource;
+import io.trino.spi.cache.CacheCapability;
+import io.trino.spi.cache.CacheKey;
+import io.trino.spi.catalog.CatalogName;
+import org.weakref.jmx.MBeanExporter;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
+import static io.trino.spi.cache.CacheCapability.CAN_EXCEED_HEAP_SIZE;
+import static java.util.Objects.requireNonNull;
+import static org.weakref.jmx.ObjectNames.generatedNameOf;
+
+public class AlluxioBlobCacheManager
+        implements BlobCacheManager
+{
+    private static final Logger log = Logger.get(AlluxioBlobCacheManager.class);
+
+    private final AlluxioCache sharedCache;
+    private final MBeanExporter exporter;
+    private final Map<CatalogName, CatalogEntry> catalogs = new ConcurrentHashMap<>();
+
+    @Inject
+    public AlluxioBlobCacheManager(AlluxioCache sharedCache, MBeanExporter exporter)
+    {
+        this.sharedCache = requireNonNull(sharedCache, "sharedCache is null");
+        this.exporter = requireNonNull(exporter, "exporter is null");
+    }
+
+    @Override
+    public boolean hasCapability(CacheCapability capability)
+    {
+        return capability == CAN_EXCEED_HEAP_SIZE;
+    }
+
+    @Override
+    public BlobCache create(CatalogName catalog, Set<CacheCapability> capabilities)
+    {
+        requireNonNull(catalog, "catalog is null");
+        return catalogs.computeIfAbsent(catalog, _ -> {
+            AlluxioCacheStats stats = new AlluxioCacheStats();
+            String name = generatedNameOf(AlluxioCacheStats.class, catalog.toString());
+            CatalogEntry entry = new CatalogEntry(name, new StatsRecordingBlobCache(sharedCache, stats));
+            try {
+                exporter.export(name, stats);
+            }
+            catch (Exception e) {
+                log.warn(e, "Failed to register AlluxioCacheStats MBean for catalog %s", catalog);
+            }
+            return entry;
+        }).blobCache();
+    }
+
+    @Override
+    public void drop(CatalogName catalog)
+    {
+        CatalogEntry entry = catalogs.remove(catalog);
+        if (entry != null) {
+            try {
+                exporter.unexport(entry.objectName());
+            }
+            catch (Exception e) {
+                log.warn(e, "Failed to unregister AlluxioCacheStats MBean for catalog %s", catalog);
+            }
+        }
+    }
+
+    @Override
+    public void shutdown()
+    {
+        Set.copyOf(catalogs.keySet()).forEach(this::drop);
+        try {
+            sharedCache.shutdown();
+        }
+        catch (Exception e) {
+            log.warn(e, "Failed to shut down shared Alluxio blob cache");
+        }
+    }
+
+    private record CatalogEntry(String objectName, BlobCache blobCache) {}
+
+    // keys arrive with the catalog name as their first component, so catalogs can share the cache
+    private record StatsRecordingBlobCache(AlluxioCache delegate, AlluxioCacheStats stats)
+            implements BlobCache
+    {
+        @Override
+        public Blob get(CacheKey key, BlobSource source)
+                throws IOException
+        {
+            return delegate.get(key, source, stats);
+        }
+
+        @Override
+        public void invalidate(CacheKey prefix)
+        {
+            delegate.invalidate(prefix);
+        }
+    }
+}

plugin/trino-blob-cache-alluxio/src/main/java/io/trino/blob/cache/alluxio/AlluxioCache.java
@@ -1,0 +1,117 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.trino.blob.cache.alluxio;
+
+import alluxio.client.file.CacheContext;
+import alluxio.client.file.URIStatus;
+import alluxio.client.file.cache.CacheManager;
+import alluxio.conf.AlluxioConfiguration;
+import alluxio.wire.FileInfo;
+import com.google.common.annotations.VisibleForTesting;
+import com.google.common.hash.HashFunction;
+import com.google.common.hash.Hasher;
+import com.google.common.hash.Hashing;
+import com.google.inject.Inject;
+import io.airlift.units.DataSize;
+import io.opentelemetry.api.trace.Tracer;
+import io.trino.spi.cache.Blob;
+import io.trino.spi.cache.BlobSource;
+import io.trino.spi.cache.CacheKey;
+import jakarta.annotation.PreDestroy;
+
+import java.io.IOException;
+
+import static com.google.common.base.Throwables.throwIfInstanceOf;
+import static com.google.common.base.Throwables.throwIfUnchecked;
+import static io.trino.plugin.base.util.Closables.closeAllSuppress;
+import static java.util.Objects.requireNonNull;
+
+public class AlluxioCache
+{
+    private static final HashFunction HASH = Hashing.murmur3_128();
+
+    private final Tracer tracer;
+    private final DataSize pageSize;
+    private final CacheManager cacheManager;
+    private final AlluxioConfiguration config;
+
+    @Inject
+    public AlluxioCache(Tracer tracer, AlluxioCacheConfig config)
+            throws IOException
+    {
+        this.tracer = requireNonNull(tracer, "tracer is null");
+        this.config = AlluxioConfigurationFactory.create(requireNonNull(config, "config is null"));
+        this.pageSize = config.getCachePageSize();
+        this.cacheManager = CacheManager.Factory.create(this.config);
+    }
+
+    public Blob get(CacheKey key, BlobSource source, AlluxioCacheStats statistics)
+            throws IOException
+    {
+        requireNonNull(key, "key is null");
+        requireNonNull(source, "source is null");
+        try {
+            URIStatus status = uriStatus(cacheIdentifier(key), source);
+            CacheManager tracingCacheManager = new TracingCacheManager(tracer, key.toString(), pageSize, cacheManager);
+            return new AlluxioBlob(tracer, source, key.toString(), status, tracingCacheManager, config, statistics);
+        }
+        catch (Throwable e) {
+            // The cache owns the source until a blob is returned, so it must not stay open
+            // when this method fails
+            closeAllSuppress(e, source);
+            throwIfInstanceOf(e, IOException.class);
+            throwIfUnchecked(e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Hashes the key components with length framing, so distinct component lists can never
+     * produce the same identifier regardless of the characters they contain.
+     */
+    @VisibleForTesting
+    static String cacheIdentifier(CacheKey key)
+    {
+        Hasher hasher = HASH.newHasher();
+        for (String component : key.components()) {
+            hasher.putInt(component.length());
+            hasher.putUnencodedChars(component);
+        }
+        return hasher.hash().toString();
+    }
+
+    // Invalidation is best effort per the BlobCache contract, and the alluxio page store cannot
+    // deliver any of it: page ids are one-way hashes of the full cache key, so a key prefix
+    // cannot be mapped back to its pages without an in-memory index over every cached blob,
+    // which is unbounded (millions of entries) and would not survive restarts anyway. Stale
+    // entries are dropped by TTL and size eviction only, so keys must be versioned (file length,
+    // lastModified) when content can be overwritten in place.
+    public void invalidate(CacheKey prefix) {}
+
+    @PreDestroy
+    public void shutdown()
+            throws Exception
+    {
+        cacheManager.close();
+    }
+
+    private static URIStatus uriStatus(String cacheIdentifier, BlobSource source)
+            throws IOException
+    {
+        FileInfo info = new FileInfo()
+                .setPath(source.toString())
+                .setLength(source.length());
+        return new URIStatus(info, CacheContext.defaults().setCacheIdentifier(cacheIdentifier));
+    }
+}

plugin/trino-blob-cache-alluxio/src/test/java/io/trino/blob/cache/alluxio/TestingBlobCache.java
@@ -1,0 +1,45 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.trino.blob.cache.alluxio;
+
+import io.trino.spi.cache.Blob;
+import io.trino.spi.cache.BlobCache;
+import io.trino.spi.cache.BlobSource;
+import io.trino.spi.cache.CacheKey;
+
+import java.io.IOException;
+
+final class TestingBlobCache
+{
+    private TestingBlobCache() {}
+
+    static BlobCache testingBlobCache(AlluxioCache cache, AlluxioCacheStats stats)
+    {
+        return new BlobCache()
+        {
+            @Override
+            public Blob get(CacheKey key, BlobSource source)
+                    throws IOException
+            {
+                return cache.get(key, source, stats);
+            }
+
+            @Override
+            public void invalidate(CacheKey key)
+            {
+                cache.invalidate(key);
+            }
+        };
+    }
+}

kobiluz/trinodb

(cherry picked from commit 2a004e67ece32460f8d3b523614bf970b6faade2)

plugin/trino-blob-cache-alluxio/src/main/java/io/trino/blob/cache/alluxio/AlluxioBlobCacheManager.java
@@ -1,0 +1,121 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.trino.blob.cache.alluxio;
+
+import com.google.inject.Inject;
+import io.airlift.log.Logger;
+import io.trino.spi.cache.Blob;
+import io.trino.spi.cache.BlobCache;
+import io.trino.spi.cache.BlobCacheManager;
+import io.trino.spi.cache.BlobSource;
+import io.trino.spi.cache.CacheCapability;
+import io.trino.spi.cache.CacheKey;
+import io.trino.spi.catalog.CatalogName;
+import org.weakref.jmx.MBeanExporter;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
+import static io.trino.spi.cache.CacheCapability.CAN_EXCEED_HEAP_SIZE;
+import static java.util.Objects.requireNonNull;
+import static org.weakref.jmx.ObjectNames.generatedNameOf;
+
+public class AlluxioBlobCacheManager
+        implements BlobCacheManager
+{
+    private static final Logger log = Logger.get(AlluxioBlobCacheManager.class);
+
+    private final AlluxioCache sharedCache;
+    private final MBeanExporter exporter;
+    private final Map<CatalogName, CatalogEntry> catalogs = new ConcurrentHashMap<>();
+
+    @Inject
+    public AlluxioBlobCacheManager(AlluxioCache sharedCache, MBeanExporter exporter)
+    {
+        this.sharedCache = requireNonNull(sharedCache, "sharedCache is null");
+        this.exporter = requireNonNull(exporter, "exporter is null");
+    }
+
+    @Override
+    public boolean hasCapability(CacheCapability capability)
+    {
+        return capability == CAN_EXCEED_HEAP_SIZE;
+    }
+
+    @Override
+    public BlobCache create(CatalogName catalog, Set<CacheCapability> capabilities)
+    {
+        requireNonNull(catalog, "catalog is null");
+        return catalogs.computeIfAbsent(catalog, _ -> {
+            AlluxioCacheStats stats = new AlluxioCacheStats();
+            String name = generatedNameOf(AlluxioCacheStats.class, catalog.toString());
+            CatalogEntry entry = new CatalogEntry(name, new StatsRecordingBlobCache(sharedCache, stats));
+            try {
+                exporter.export(name, stats);
+            }
+            catch (Exception e) {
+                log.warn(e, "Failed to register AlluxioCacheStats MBean for catalog %s", catalog);
+            }
+            return entry;
+        }).blobCache();
+    }
+
+    @Override
+    public void drop(CatalogName catalog)
+    {
+        CatalogEntry entry = catalogs.remove(catalog);
+        if (entry != null) {
+            try {
+                exporter.unexport(entry.objectName());
+            }
+            catch (Exception e) {
+                log.warn(e, "Failed to unregister AlluxioCacheStats MBean for catalog %s", catalog);
+            }
+        }
+    }
+
+    @Override
+    public void shutdown()
+    {
+        Set.copyOf(catalogs.keySet()).forEach(this::drop);
+        try {
+            sharedCache.shutdown();
+        }
+        catch (Exception e) {
+            log.warn(e, "Failed to shut down shared Alluxio blob cache");
+        }
+    }
+
+    private record CatalogEntry(String objectName, BlobCache blobCache) {}
+
+    // keys arrive with the catalog name as their first component, so catalogs can share the cache
+    private record StatsRecordingBlobCache(AlluxioCache delegate, AlluxioCacheStats stats)
+            implements BlobCache
+    {
+        @Override
+        public Blob get(CacheKey key, BlobSource source)
+                throws IOException
+        {
+            return delegate.get(key, source, stats);
+        }
+
+        @Override
+        public void tryInvalidate(CacheKey prefix)
+        {
+            delegate.tryInvalidate(prefix);
+        }
+    }
+}

plugin/trino-blob-cache-alluxio/src/main/java/io/trino/blob/cache/alluxio/AlluxioCache.java
@@ -1,0 +1,117 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.trino.blob.cache.alluxio;
+
+import alluxio.client.file.CacheContext;
+import alluxio.client.file.URIStatus;
+import alluxio.client.file.cache.CacheManager;
+import alluxio.conf.AlluxioConfiguration;
+import alluxio.wire.FileInfo;
+import com.google.common.annotations.VisibleForTesting;
+import com.google.common.hash.HashFunction;
+import com.google.common.hash.Hasher;
+import com.google.common.hash.Hashing;
+import com.google.inject.Inject;
+import io.airlift.units.DataSize;
+import io.opentelemetry.api.trace.Tracer;
+import io.trino.spi.cache.Blob;
+import io.trino.spi.cache.BlobSource;
+import io.trino.spi.cache.CacheKey;
+import jakarta.annotation.PreDestroy;
+
+import java.io.IOException;
+
+import static com.google.common.base.Throwables.throwIfInstanceOf;
+import static com.google.common.base.Throwables.throwIfUnchecked;
+import static io.trino.plugin.base.util.Closables.closeAllSuppress;
+import static java.util.Objects.requireNonNull;
+
+public class AlluxioCache
+{
+    private static final HashFunction HASH = Hashing.murmur3_128();
+
+    private final Tracer tracer;
+    private final DataSize pageSize;
+    private final CacheManager cacheManager;
+    private final AlluxioConfiguration config;
+
+    @Inject
+    public AlluxioCache(Tracer tracer, AlluxioCacheConfig config)
+            throws IOException
+    {
+        this.tracer = requireNonNull(tracer, "tracer is null");
+        this.config = AlluxioConfigurationFactory.create(requireNonNull(config, "config is null"));
+        this.pageSize = config.getCachePageSize();
+        this.cacheManager = CacheManager.Factory.create(this.config);
+    }
+
+    public Blob get(CacheKey key, BlobSource source, AlluxioCacheStats statistics)
+            throws IOException
+    {
+        requireNonNull(key, "key is null");
+        requireNonNull(source, "source is null");
+        try {
+            URIStatus status = uriStatus(cacheIdentifier(key), source);
+            CacheManager tracingCacheManager = new TracingCacheManager(tracer, key.toString(), pageSize, cacheManager);
+            return new AlluxioBlob(tracer, source, key.toString(), status, tracingCacheManager, config, statistics);
+        }
+        catch (Throwable e) {
+            // The cache owns the source until a blob is returned, so it must not stay open
+            // when this method fails
+            closeAllSuppress(e, source);
+            throwIfInstanceOf(e, IOException.class);
+            throwIfUnchecked(e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Hashes the key components with length framing, so distinct component lists can never
+     * produce the same identifier regardless of the characters they contain.
+     */
+    @VisibleForTesting
+    static String cacheIdentifier(CacheKey key)
+    {
+        Hasher hasher = HASH.newHasher();
+        for (String component : key.components()) {
+            hasher.putInt(component.length());
+            hasher.putUnencodedChars(component);
+        }
+        return hasher.hash().toString();
+    }
+
+    // Invalidation is best effort per the BlobCache contract, and the alluxio page store cannot
+    // deliver any of it: page ids are one-way hashes of the full cache key, so a key prefix
+    // cannot be mapped back to its pages without an in-memory index over every cached blob,
+    // which is unbounded (millions of entries) and would not survive restarts anyway. Stale
+    // entries are dropped by TTL and size eviction only, so keys must be versioned (file length,
+    // lastModified) when content can be overwritten in place.
+    public void tryInvalidate(CacheKey prefix) {}
+
+    @PreDestroy
+    public void shutdown()
+            throws Exception
+    {
+        cacheManager.close();
+    }
+
+    private static URIStatus uriStatus(String cacheIdentifier, BlobSource source)
+            throws IOException
+    {
+        FileInfo info = new FileInfo()
+                .setPath(source.toString())
+                .setLength(source.length());
+        return new URIStatus(info, CacheContext.defaults().setCacheIdentifier(cacheIdentifier));
+    }
+}

plugin/trino-blob-cache-alluxio/src/test/java/io/trino/blob/cache/alluxio/TestingBlobCache.java
@@ -1,0 +1,45 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.trino.blob.cache.alluxio;
+
+import io.trino.spi.cache.Blob;
+import io.trino.spi.cache.BlobCache;
+import io.trino.spi.cache.BlobSource;
+import io.trino.spi.cache.CacheKey;
+
+import java.io.IOException;
+
+final class TestingBlobCache
+{
+    private TestingBlobCache() {}
+
+    static BlobCache testingBlobCache(AlluxioCache cache, AlluxioCacheStats stats)
+    {
+        return new BlobCache()
+        {
+            @Override
+            public Blob get(CacheKey key, BlobSource source)
+                    throws IOException
+            {
+                return cache.get(key, source, stats);
+            }
+
+            @Override
+            public void tryInvalidate(CacheKey key)
+            {
+                cache.tryInvalidate(key);
+            }
+        };
+    }
+}